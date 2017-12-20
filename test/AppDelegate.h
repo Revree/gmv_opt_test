1430f476ecacc9f5ad988dab18cf3e0c1c24eb52
Andrea Chen <andreachen@kunals-macbook-pro.local>
2017-12-19 23:23:49 -0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Andrea Chen on 12/19/17.
+//  Copyright © 2017 Andrea Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Andrea Chen on 12/19/17.
+//  Copyright © 2017 Andrea Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
